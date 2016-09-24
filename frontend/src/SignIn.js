3cb2ebfca7755c53d40347b2316877ab64d1c0b3
Colby Aley <colby@aley.me>
2016-09-24 16:42:05 -0700

    Add connect to foursquare button, styling tweaks

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './App.css';
+import connectButton from './connect.png';
 
 class AuthButton extends React.Component {
   constructor(props) {
@@ -13,9 +15,11 @@ class AuthButton extends React.Component {
 
   render() {
     return (
-      <a href='http://127.0.0.1:4200/authenticate'>
-        auth with foursquare
-      </a>
+      <div className="SignIn-container">
+        <a href='http://127.0.0.1:4200/authenticate'>
+          <img className="SignIn-button" src={ connectButton } alt="Connect with Foursquare"/>
+        </a>
+      </div>
     );
   }
 }
